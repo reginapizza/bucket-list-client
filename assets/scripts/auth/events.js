aa0f1638375006f4d1482181b15515e73b2be3d0
mwasswa <m_wasswa@yahoo.com>
2019-11-05 15:44:15 -0500

    Edits to ui.js

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,50 @@
+'use strict'
+const ui = require('./ui.js')
+const api = require('./api.js')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signUp(formData)
+    .then(() => api.signIn(formData))
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signIn(formData)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  api.changePassword(formData)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onChangePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
