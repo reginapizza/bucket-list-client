54c4dbf107b3db0d25688a654670173c87f31a47
reginapizza <reginaelyse@gmail.com>
2019-11-05 16:49:47 -0500

    Added AJAX calls to api.js
    This is for all crud actions

---
+++ assets/scripts/items/api.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const createItem = function (formData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/items',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+// const getAllItems = function () {
+//   return $.ajax({
+//     url: config.apiUrl + '/items',
+//     method: 'GET',
+//     data: {
+//
+//     }
+//   })
+// }
+
+const getMyItems = function () {
+  return $.ajax({
+    url: config.apiUrl + '/items',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+
+    }
+  })
+}
+
+const showItem = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/items/' + formData.item.id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const updateItem = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/items/' + formData.item.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+const deleteItem = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/items/' + formData.item.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: formData
+  })
+}
+
+module.exports = {
+  createItem,
+  // getAllItems,
+  getMyItems,
+  showItem,
+  updateItem,
+  deleteItem
+}
